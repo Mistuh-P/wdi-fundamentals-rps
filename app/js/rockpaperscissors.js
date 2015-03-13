74b39c230fe8b237565a0a0aad86edbbfc2590b9
Mistuh-P <mistuhpat@gmail.com>
2015-03-13 13:12:27 -0700

    Update rockpaperscissors.js

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -68,7 +68,14 @@ function playToFive() {
     var playerWins = 0;
     var computerWins = 0;
     // Write code that plays 'Rock, Paper, Scissors' until either the player or the computer has won five times.
-    /* YOUR CODE HERE */
+    while (playerWins < 5 || computerWins < 5){
+        if (getWinner = 'player'){
+            playerWins + 1;
+        }
+        else if (getWinner = 'computer'){
+            computerWins + 1;
+        }
+    }
     return [playerWins, computerWins];
 }
 
