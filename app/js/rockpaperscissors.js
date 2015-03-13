0050233bbd07adcee78760e57973f01084a95595
Mistuh-P <mistuhpat@gmail.com>
2015-03-13 12:31:02 -0700

    Update rockpaperscissors.js
    Added functionality to getWinner

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -40,7 +40,26 @@ function getWinner(playerMove,computerMove) {
     // Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
     // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
     // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
-    /* YOUR CODE HERE */
+    if (playerMove = 'rock' && computerMove = 'scissors'){
+        winner = 'player'
+    }
+    if (playerMove = 'rock' && computerMove = 'paper'){
+        winner = 'computer'
+    }
+    if (playerMove = 'scissors' && computerMove = 'paper'){
+        winner = 'player'
+    }
+    if (playerMove = 'scissors' && computerMove = 'rock'){
+        winner = 'computer'
+    }
+    if (playerMove = 'paper' && computerMove = 'rock'){
+        winner = 'player'
+    }
+    if (playerMove = 'paper' && computerMove = 'scissors'){
+        winner = 'computer'
+        else {
+            winner = 'tie'
+        }
     return winner;
 }
 
